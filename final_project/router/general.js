4118e66d8ff682d00e6fe7f76fde7959e4a369a2
Teylor Feliz <teylormanuel@gmail.com>
2023-10-02 19:36:30 -0600

    task 5 finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,8 +57,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if(book) {
+    return res.status(200).json(book.reviews);
+  }
+    return res.status(404).json({message: "Item no found"})
 });
 
 module.exports.general = public_users;
