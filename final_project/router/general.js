baf04867b4eadb154fabd5402a4d75c85b074f63
Teylor Feliz <teylormanuel@gmail.com>
2023-10-02 19:05:47 -0600

    task 2 finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,17 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-
   return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if(book) {
+    return res.status(200).json(book);
+  }
+    return res.status(404).json({message: "Item no found"})
  });
   
 // Get book details based on author
